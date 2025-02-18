98e106656f063844772ee1c7e468eaba1a23b04a
Bozhidar Petrov <g459x19@gmail.com>
2025-02-18 03:37:04 -0600

    Added random book on home screen that changes upon refresh

--- src/app/core/components/home/home.component.ts
+++ src/app/core/components/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Store } from '@ngrx/store';
 import {
@@ -9,16 +9,23 @@ import {
   selectValidationErrors,
 } from '../../../auth/store/reducers';
 import { combineLatest } from 'rxjs';
+import { BookFromMongoose } from '../../../book/components/types/bookFromMongoose';
+import { HttpClient } from '@angular/common/http';
+import { BookCardComponent } from '../../../book/components/book-card/book-card.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, BookCardComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
-export class HomeComponent {
-  constructor(private store: Store) {}
+export class HomeComponent implements OnInit {
+  constructor(private store: Store, private http: HttpClient) {}
+
+  allBooks: BookFromMongoose[] = [];
+
+  randomBook: BookFromMongoose | undefined;
 
   data$ = combineLatest({
     user: this.store.select(selectUser),
@@ -27,4 +34,17 @@ export class HomeComponent {
     backendErrors: this.store.select(selectValidationErrors),
     isLoggedIn: this.store.select(selectIsLoggedIn),
   });
+
+  ngOnInit(): void {
+    this.http
+      .get<BookFromMongoose[]>('http://localhost:5000/api/books/all')
+      .subscribe({
+        next: (books) => {
+          this.allBooks = books;
+          let random = Math.floor(Math.random() * this.allBooks.length);
+          this.randomBook = this.allBooks[random];
+          console.log(this.randomBook);
+        },
+      });
+  }
 }

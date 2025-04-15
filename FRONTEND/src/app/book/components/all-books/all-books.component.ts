ad5ce6e8ccd06f238b1df6b707ff099a87b96aa5
Bozhidar Petrov <g459x19@gmail.com>
2025-04-15 06:15:35 -0500

    Pagination on All Books

--- FRONTEND/src/app/book/components/all-books/all-books.component.ts
+++ FRONTEND/src/app/book/components/all-books/all-books.component.ts
@@ -1,24 +1,60 @@
 import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { Component, Injectable, OnInit } from '@angular/core';
 import { BookCardComponent } from '../book-card/book-card.component';
 import { BookFromMongoose } from '../../types/bookFromMongoose';
+import { MatPaginatorModule, PageEvent } from '@angular/material/paginator';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-all-books',
   standalone: true,
-  imports: [CommonModule, BookCardComponent],
+  imports: [CommonModule, BookCardComponent, MatPaginatorModule],
   templateUrl: './all-books.component.html',
   styleUrl: './all-books.component.css',
 })
+@Injectable()
 export class AllBooksComponent implements OnInit {
   constructor(private http: HttpClient) {}
 
+  startIndex: number | undefined;
+  endIndex: number | undefined;
+
   allBooks: BookFromMongoose[] = [];
+  booksOfGivenPage: BookFromMongoose[] = [];
+
+  currentPage = 0;
+  handlePageEvent(pageEvent: PageEvent) {
+    console.log('handlePageEvent', pageEvent);
+    this.currentPage = pageEvent.pageIndex;
+    this.fillBooksOfGivenPageArr();
+  }
+
+  fillBooksOfGivenPageArr() {
+    if (this.currentPage === 0) {
+      this.booksOfGivenPage = this.allBooks.slice(0, 4);
+    } else {
+      this.startIndex = this.currentPage * 4;
+      this.endIndex = (this.currentPage + 1) * 4 - 1;
+
+      this.booksOfGivenPage = this.allBooks.slice(
+        this.startIndex,
+        this.endIndex
+      );
+    }
+  }
 
   ngOnInit(): void {
     this.http
       .get<BookFromMongoose[]>('http://localhost:5000/api/books/all')
-      .subscribe((books) => (this.allBooks = books));
+      .subscribe({
+        next: (books) => {
+          this.allBooks = books;
+          this.fillBooksOfGivenPageArr();
+        },
+        error: (err) => {
+          console.log(`Error: ${err}`);
+        },
+      });
   }
 }

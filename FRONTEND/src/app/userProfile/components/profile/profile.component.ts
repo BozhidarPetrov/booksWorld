1783755887d989c7590802fe4322a9f296af2296
Bozhidar Petrov <g459x19@gmail.com>
2025-04-22 04:46:09 -0500

    CSS changes on Profile - My Books and Liked Books

--- FRONTEND/src/app/userProfile/components/profile/profile.component.ts
+++ FRONTEND/src/app/userProfile/components/profile/profile.component.ts
@@ -7,11 +7,18 @@ import { HttpClient } from '@angular/common/http';
 import { BookCardSmallComponent } from '../../../book/components/book-card-small/book-card-small.component';
 import { BookFromMongoose } from '../../../book/types/bookFromMongoose';
 import { RouterLink } from '@angular/router';
+import { MatPaginatorModule, PageEvent } from '@angular/material/paginator';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [CommonModule, BookCardSmallComponent, RouterLink],
+  imports: [
+    CommonModule,
+    BookCardSmallComponent,
+    RouterLink,
+    MatPaginatorModule,
+  ],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.css',
 })
@@ -20,8 +27,32 @@ export class ProfileComponent implements OnInit {
 
   allBooks: BookFromMongoose[] = [];
   filteredBooks: BookFromMongoose[] = [];
+  filteredBooksPerPage: BookFromMongoose[] = [];
   toggleName: string = '';
   userId: String | undefined = '';
+  startIndex: number | undefined;
+  endIndex: number | undefined;
+
+  currentPage = 0;
+  handlePageEvent(pageEvent: PageEvent) {
+    console.log('handlePageEvent', pageEvent);
+    this.currentPage = pageEvent.pageIndex;
+    this.fillBooksOfGivenPageArr();
+  }
+
+  fillBooksOfGivenPageArr() {
+    if (this.currentPage === 0) {
+      this.filteredBooksPerPage = this.filteredBooks.slice(0, 4); //This logic is based on a grid with 1 row and 4 cells on it, showing 1-4 items per page
+    } else {
+      this.startIndex = this.currentPage * 4; //This logic is based on up to 4 items per page, in this particular case this will be a fixed number, due to styling reasons
+      this.endIndex = (this.currentPage + 1) * 4 - 1; //This logic is based on up to 4 items per page, in this particular case this will be a fixed number, due to styling reasons
+
+      this.filteredBooksPerPage = this.filteredBooks.slice(
+        this.startIndex,
+        this.endIndex + 1
+      );
+    }
+  }
 
   data$ = combineLatest({
     user: this.store.select(selectUser),
@@ -32,9 +63,11 @@ export class ProfileComponent implements OnInit {
       (book) => book.owner._id === this.userId
     );
     this.toggleName = 'myBooks';
+    this.fillBooksOfGivenPageArr() 
   }
 
   favoriteBooks(): void {
+    
     const id = this.userId?.toString();
 
     let favoriteBooks: BookFromMongoose[] = [];
@@ -49,6 +82,7 @@ export class ProfileComponent implements OnInit {
     });
     this.filteredBooks = favoriteBooks;
     this.toggleName = 'favoriteBooks';
+    this.fillBooksOfGivenPageArr() 
   }
 
   ngOnInit(): void {
@@ -63,6 +97,7 @@ export class ProfileComponent implements OnInit {
       .subscribe({
         next: (books) => {
           this.allBooks = books;
+          
         },
       });
   }
